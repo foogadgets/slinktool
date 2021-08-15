3939cb8f23830b2c4c56d5cf919a2a6812d984ca
Niclas Forslund <foogadgets@gmail.com>
2021-08-15 13:36:09 +0200

    New web layout

--- html-generator/gen_compact_index_file.sh
+++ html-generator/gen_compact_index_file.sh
@@ -4,9 +4,10 @@ TMPFILE='mktemp' || exit 1
 printf "static const char index_html[] PROGMEM = R\"rawliteral(" > ../webpages.h
 cat http/main_base_ip_arduino.js > $TMPFILE
 cat dist/main.js >> $TMPFILE
-awk '/HEPPELEPPE/{system("cat '$TMPFILE'");next}1' http/index_base.html | tr -d '\011\012\015' | tr -s " " >> ../webpages.h
+awk '/GURKMAJO/{system("cat 'http/layout.css'");next}1' http/index_base.html >> webpage.tmp
+awk '/HEPPELEPPE/{system("cat '$TMPFILE'");next}1' webpage.tmp | tr -d '\011\012\015' | tr -s " " >> ../webpages.h
 printf ")rawliteral\";\n" >> ../webpages.h
-rm -rf $TMPFILE
+rm -rf $TMPFILE webpage.tmp
 
 TMPFILE='mktemp' || exit 1
 printf "static const char login_template[] PROGMEM = R\"rawliteral(" >> ../webpages.h
@@ -21,12 +22,9 @@ printf ")rawliteral\";\n" >> ../webpages.h
 
 
 
-#printf "static const char favicon[] PROGMEM = R\"rawliteral(" > ../favicon.h
-#cat http/favicon.ico >> ../favicon.h
-#printf ")rawliteral\";\n" >> ../favicon.h
-
 TMPFILE='mktemp' || exit 1
 cat http/main_base_ip.js > $TMPFILE
 cat dist/main.js >> $TMPFILE
-awk '/HEPPELEPPE/{system("cat '$TMPFILE'");next}1' http/index_base.html | tr -d '\011\012\015' | tr -s " " > webpage.html
-rm -rf $TMPFILE
+awk '/GURKMAJO/{system("cat 'http/layout.css'");next}1' http/index_base.html > webpage.tmp
+awk '/HEPPELEPPE/{system("cat '$TMPFILE'");next}1' webpage.tmp | tr -d '\011\012\015' | tr -s " " > webpage.html
+rm -rf $TMPFILE webpage.tmp
